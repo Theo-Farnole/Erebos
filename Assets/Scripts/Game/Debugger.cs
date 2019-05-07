252f67ea7b6099d35d269c44cf6d4ee375cfde34
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2019-05-07 11:19:24 +0200

    Add Debugger
    > allow to restart level

---
+++ Assets/Scripts/Game/Debugger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GamepadInput;
+using UnityEngine.SceneManagement;
+
+public class Debugger : MonoBehaviour
+{
+    void Update()
+    {
+        if (GamePad.GetButtonUp(GamePad.Button.Start, GamePad.Index.One))
+        {
+            int scene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(scene, LoadSceneMode.Single);
+        }
+    }
+}
