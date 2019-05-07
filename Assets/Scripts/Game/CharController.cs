03a680c16107dc91e93592668e1fb43b7a4bbc4c
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2019-05-07 14:00:49 +0200

    Code preparation for sticked jump
    + code cleaning

--- Assets/Scripts/Game/CharController.cs
+++ Assets/Scripts/Game/CharController.cs
@@ -13,21 +13,29 @@ public class CharController : MonoBehaviour
     [Header("Model settings")]
     [SerializeField] private Transform _model = null;
 
-    // movements variables
+    // inputs variables
     private bool _jumpInput = false;
     private float _horizontal = 0;
+
+    // movements variables
+    private bool _isSticked = false;
     private int _jumpsAvailable = 0;
 
     // cached variables
-    private float _distToGround;
     private Rigidbody _rigidbody;
+    private Collider _collider;
+    private float _distToGround;
+    private float _distToSide;
     #endregion
 
     #region MonoBehaviour Callbacks
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _collider = GetComponent<Collider>();
+
         _distToGround = GetComponent<Collider>().bounds.extents.y;
+        _distToSide = GetComponent<Collider>().bounds.extents.x;
     }
 
     void Update()
@@ -37,8 +45,9 @@ public class CharController : MonoBehaviour
 
     void FixedUpdate()
     {
-        ProcessJumpInput();
         ProcessRunInput();
+        ProcessSticking();
+        NormalJump();
     }
     #endregion
 
@@ -55,26 +64,18 @@ public class CharController : MonoBehaviour
         {
             _jumpInput = false;
         }
-
     }
 
     private void ProcessRunInput()
     {
         Vector3 speed = Vector3.right * _horizontal * _data.Speed;
-        Vector3 vel = _rigidbody.velocity;
 
-        // on ground
-        if (_jumpsAvailable == MAX_JUMPS) 
+        if (!_isSticked)
         {
+            Vector3 vel = _rigidbody.velocity;
             vel.x = speed.x;
+            _rigidbody.velocity = vel;
         }
-        // air control
-        else
-        {
-            vel.x = speed.x; // tmp var
-            //vel.x += speed.x * Time.fixedDeltaTime * _data.AirControl;
-        }
-        _rigidbody.velocity = vel;
 
         // turn the character where he runs
         if (_horizontal != 0)
@@ -85,24 +86,57 @@ public class CharController : MonoBehaviour
         }
     }
 
+    private void ProcessSticking()
+    {
+        // determinate direction of raycast ...
+        Vector3 dir = Vector3.one;
+
+        if (_horizontal < 0)
+        {
+            dir = Vector3.left;
+        }
+        else
+        {
+            dir = Vector3.right;
+        }
+
+        // ... then raycast ...
+        _isSticked = Physics.Raycast(transform.position, dir, _distToSide + 0.1f);
+
+        // .. reset velocity if raycast is true
+        if (_isSticked)
+        {
+            _rigidbody.velocity = Vector3.one;
+        }
+    }
+
+    #region Jump Methods
     private void ProcessJumpInput()
     {
+        // check if we should reset jumps count.
         if (IsGrounded())
         {
-            if (_jumpsAvailable != MAX_JUMPS)
-            {
-                Debug.Log("JUMP RESET");
-            }
-
             _jumpsAvailable = MAX_JUMPS;
         }
 
-        // manage jump input
+        if (_isSticked)
+        {
+            StickedJump();
+        }
+        else
+        {
+            NormalJump();
+        }
+    }
+
+
+
+    private void NormalJump()
+    {
         if (_jumpInput && _jumpsAvailable > 0)
         {
             _jumpInput = false;
 
-            Debug.Log("JUMP");
             if (_jumpsAvailable == 2)
             {
                 _rigidbody.AddForce(Vector3.up * _data.FirstJumpForce, ForceMode.Impulse);
@@ -117,8 +151,14 @@ public class CharController : MonoBehaviour
         }
     }
 
+    private void StickedJump()
+    {
+        Debug.LogWarning("StickedJump not implemented");
+    }
+
     private bool IsGrounded()
     {
         return Physics.Raycast(transform.position, -Vector3.up, _distToGround + 0.1f);
     }
+    #endregion
 }
