114243eab46523bc95e757fb05d5f4ab2abdfb53
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2019-05-08 20:14:16 +0200

    Options' persistence

---
+++ Assets/Scripts/Persistence/OptionsData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class OptionsData
+{
+    public bool enableVSync = false;
+    public bool displayFPS = false;
+
+    public float soundGeneral = 1f;
+    public float soundMusique = 1f;
+    public float soundAmbiance = 1f;
+}
+
---
+++ Assets/Scripts/Persistence/SaveSystem.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public static class SaveSystem
+{
+    public static OptionsData optionsData;
+    readonly static string pathOptions = "options_data.gd";
+
+
+    public static void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+
+        FileStream file = File.Create(Application.persistentDataPath + "/" + pathOptions);
+        bf.Serialize(file, optionsData);
+
+        file.Close();
+    }
+
+    public static void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/" + pathOptions))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+
+            FileStream file = File.Open(Application.persistentDataPath + "/" + pathOptions, FileMode.Open);
+            optionsData = (OptionsData)bf.Deserialize(file);
+
+            file.Close();
+        }
+        else
+        {
+            optionsData = new OptionsData();
+        }
+    }
+}
