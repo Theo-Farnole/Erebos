5e1a0c69cc645430cb96de17c7206332f19e6396
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2019-05-09 14:59:14 +0200

    First daft: Black Singularity

---
+++ Assets/Scripts/Game/Singularities/BlackSingularity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackSingularity : MonoBehaviour
+{
+    #region Fields
+    [SerializeField] private BlackSingularityData _data;
+    [Space]
+    [SerializeField] private DrawCircle _rangeFeedback;
+    #endregion
+
+    #region MonoBehaviour Callbacks
+    void Awake()
+    {
+        _rangeFeedback.xradius = _data.Range;
+        _rangeFeedback.yradius = _data.Range;
+    }
+    #endregion
+}
---
+++ Assets/Scripts/Game/Singularities/BlackSingularityData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Erebos/Black Singularity")]
+public class BlackSingularityData : ScriptableObject
+{
+    [SerializeField] private float _range;
+
+    public float Range { get => _range; }
+}
