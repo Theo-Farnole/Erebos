7b2687b3c52fd0dfb1bd6594a4c246fbe4887fe6
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2019-05-10 13:24:32 +0200

    Singularity works 😎

--- Assets/Scripts/Game/Singularities/Data/SingularityData.cs
+++ Assets/Scripts/Game/Singularities/Data/SingularityData.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Erebos/Singularity")]
 public class SingularityData : ScriptableObject
 {
-    [SerializeField] private float _range = 3f;
-    [SerializeField] private float _reactionForce = 3f;
+    [SerializeField] private float _range = 8f;
+    [SerializeField] private float _minRange = 3f;
+    [SerializeField] private float _reactionForce = 20000f;
 
     public float Range { get => _range; }
+    public float MinRange { get => _minRange; }
     public float ReactionForce { get => _reactionForce; }
 
 #if UNITY_EDITOR
--- Assets/Scripts/Game/Singularities/Singularity.cs
+++ Assets/Scripts/Game/Singularities/Singularity.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using GamepadInput;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,6 +9,7 @@ public class Singularity : MonoBehaviour
     [SerializeField] private SingularityData _data;
     [Space]
     [SerializeField] private DrawCircle _rangeFeedback;
+    [SerializeField] private bool _noOverrideForce = true;
 
     private bool _isPlayerInRange = false;
     private Transform _player;
@@ -25,10 +27,34 @@ public class Singularity : MonoBehaviour
 
         if (_isPlayerInRange)
         {
-            Vector3 dir = (transform.position - _player.position).normalized;
-            _player.GetComponent<Rigidbody>().AddForce(_data.ReactionForce * dir * Time.deltaTime);
+            if (Vector3.Distance(transform.position, _player.position) >= _data.MinRange)
+            {
+                Vector3 dir = (transform.position - _player.position).normalized;
+                _player.GetComponent<Rigidbody>().AddForce(_data.ReactionForce * dir * Time.deltaTime);
 
-            Debug.DrawRay(transform.position, dir * _data.ReactionForce, Color.red);
+                Debug.DrawRay(transform.position, dir * _data.ReactionForce, Color.red);
+            }
+            // rotate around circle
+            else
+            {
+                
+
+                // reset vel
+                _player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+                int anglePerSecond = 360;
+                float horizontal = GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.One).x * Time.deltaTime * anglePerSecond;
+
+                Vector3 dir = _player.transform.position - transform.position;
+                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+                Debug.Log("angle > " + angle);
+
+                angle += horizontal;
+
+                Vector3 offset = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+                _player.transform.position = transform.position + offset * _data.MinRange;
+            }
         }
     }
     #endregion
