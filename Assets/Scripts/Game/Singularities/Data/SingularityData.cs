28c46be9db5ff2c74154a58e43c15921b46a47ce
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2019-05-10 13:37:08 +0200

    AnglePerSecond in SingularityData
    Cleaning Update function

--- Assets/Scripts/Game/Singularities/Data/SingularityData.cs
+++ Assets/Scripts/Game/Singularities/Data/SingularityData.cs
@@ -7,11 +7,14 @@ public class SingularityData : ScriptableObject
 {
     [SerializeField] private float _range = 8f;
     [SerializeField] private float _minRange = 3f;
+    [Space]
     [SerializeField] private float _reactionForce = 20000f;
+    [SerializeField] private int _anglePerSecond = 360;
 
     public float Range { get => _range; }
     public float MinRange { get => _minRange; }
     public float ReactionForce { get => _reactionForce; }
+    public int AnglePerSecond { get => _anglePerSecond; }
 
 #if UNITY_EDITOR
     void OnValidate()
@@ -21,9 +24,8 @@ public class SingularityData : ScriptableObject
         foreach (var s in singularities)
         {
             s.UpdateRangeFeedback();
-        }
+        } 
     }
 #endif
-
 }
 
--- Assets/Scripts/Game/Singularities/Singularity.cs
+++ Assets/Scripts/Game/Singularities/Singularity.cs
@@ -29,35 +29,41 @@ public class Singularity : MonoBehaviour
         {
             if (Vector3.Distance(transform.position, _player.position) >= _data.MinRange)
             {
-                Vector3 dir = (transform.position - _player.position).normalized;
-                _player.GetComponent<Rigidbody>().AddForce(_data.ReactionForce * dir * Time.deltaTime);
-
-                Debug.DrawRay(transform.position, dir * _data.ReactionForce, Color.red);
+                AttractPlayer();
             }
-            // rotate around circle
             else
             {
-                
-
-                // reset vel
-                _player.GetComponent<Rigidbody>().velocity = Vector3.zero;
-
-                int anglePerSecond = 360;
-                float horizontal = GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.One).x * Time.deltaTime * anglePerSecond;
-
-                Vector3 dir = _player.transform.position - transform.position;
-                float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-
-                Debug.Log("angle > " + angle);
-
-                angle += horizontal;
-
-                Vector3 offset = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
-                _player.transform.position = transform.position + offset * _data.MinRange;
+                RotateAroundCircle();
             }
         }
     }
     #endregion
+
+    void AttractPlayer()
+    {
+        Vector3 dir = (transform.position - _player.position).normalized;
+        _player.GetComponent<Rigidbody>().AddForce(_data.ReactionForce * dir * Time.deltaTime);
+
+        Debug.DrawRay(transform.position, dir * _data.ReactionForce, Color.red);
+    }
+
+    void RotateAroundCircle()
+    {
+        // reset vel
+        _player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+        // process input
+        float horizontal = GamePad.GetAxis(GamePad.Axis.LeftStick, GamePad.Index.One).x * Time.deltaTime * _data.AnglePerSecond;
+
+        Vector3 dir = _player.transform.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        angle += horizontal;
+
+        // apply new position
+        Vector3 offset = new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
+        _player.transform.position = transform.position + offset * _data.MinRange;
+    }
 
     void IsPlayerInRange()
     {
@@ -84,14 +90,11 @@ public class Singularity : MonoBehaviour
 
     public void UpdateRangeFeedback()
     {
-        Debug.Log("UpdateRangeFeedback()");
-
         if (_rangeFeedback == null)
         {
             Debug.LogError(transform.name + " has no range feedback dragged!");
             return;
         }
-
 
         _rangeFeedback.xradius = _data.Range;
         _rangeFeedback.yradius = _data.Range;
