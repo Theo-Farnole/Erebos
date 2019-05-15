ba1d566eae0296db8f50dfb0ef997aab2c4331b1
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2019-05-15 14:58:54 +0200

    Active Checkpoint feedback

--- Assets/Scripts/Game/Character/CharDeath.cs
+++ Assets/Scripts/Game/Character/CharDeath.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class CharDeath : MonoBehaviour
 {
     #region Fields
-    [HideInInspector] public Vector3 currentCheckpoint;
+    [SerializeField] private Material _materialNotActive;
+    [SerializeField] private Material _materialActive;
+
+    [HideInInspector] public Transform currentCheckpoint;
+
     #endregion
 
     #region MonoBehaviour Callbacks
     private void Awake()
     {
-        currentCheckpoint = transform.position;
+        currentCheckpoint = transform;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -35,12 +39,32 @@ public class CharDeath : MonoBehaviour
         CharControllerManager.Instance.Attracted = false;
         CharController.Instance.ResetMovements();
 
-        transform.position = (Vector2)currentCheckpoint;
+        transform.position = (Vector2)currentCheckpoint.position;
     }
 
     private void SetNewCheckpoint(Collider other)
     {
-        Debug.Log("New checkpoint is " + other.name);
-        currentCheckpoint = other.transform.position;
+        // set not active material to the old checkpoint 
+        if (currentCheckpoint != transform)
+        {
+            var oldMat = currentCheckpoint.GetChild(0).GetComponentsInChildren<MeshRenderer>();
+
+            foreach (var m in oldMat)
+            {
+                m.material = _materialNotActive;
+            }
+        }
+
+
+        // change current checkpoint
+        currentCheckpoint = other.transform;
+
+        // then apply new material
+        MeshRenderer[] newMat = currentCheckpoint.GetChild(0).GetComponentsInChildren<MeshRenderer>();
+
+        foreach (var m in newMat)
+        {
+            m.material = _materialActive;
+        }
     }
 }
