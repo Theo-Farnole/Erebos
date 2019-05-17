d0d3961e400de1d476cfb04069d85c554ab1d986
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2019-05-17 09:30:14 +0200

    Normal state for singularity

--- Assets/Scripts/Game/Singularities/SingularityEntity.cs
+++ Assets/Scripts/Game/Singularities/SingularityEntity.cs
@@ -7,6 +7,7 @@ public class SingularityEntity : MonoBehaviour
 {
     [SerializeField] private BlackSingularity _blackSingularity;
     [SerializeField] private WhiteSingularity _whiteSingularity;
+    [SerializeField] private GameObject _normalSingularity;
 
     void Update()
     {
@@ -15,16 +16,19 @@ public class SingularityEntity : MonoBehaviour
             case CharControllerSingularity.Form.Normal:
                 _blackSingularity.gameObject.SetActive(false);
                 _whiteSingularity.gameObject.SetActive(false);
+                _normalSingularity.SetActive(true);
                 break;
 
             case CharControllerSingularity.Form.Ethereal:
                 _blackSingularity.gameObject.SetActive(false);
                 _whiteSingularity.gameObject.SetActive(true);
+                _normalSingularity.SetActive(false);
                 break;
 
             case CharControllerSingularity.Form.Void:
                 _blackSingularity.gameObject.SetActive(true);
                 _whiteSingularity.gameObject.SetActive(false);
+                _normalSingularity.SetActive(false);
                 break;
         }
     }
