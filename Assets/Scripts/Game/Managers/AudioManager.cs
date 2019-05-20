a9fa5b39cbbb9482b8ffdba170f65a25e91180c9
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2019-05-20 16:38:05 +0200

    Add AudioManager

---
+++ Assets/Scripts/Game/Managers/AudioManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public enum SoundGeneral { };
+    public enum SoundMusic { };
+    public enum SoundAmbiance { };
+
+    public void PlaySoundGeneral(SoundGeneral sound)
+    {
+        float volume = SaveSystem.optionsData.soundGeneral;
+
+        switch (sound)
+        {
+            default:
+                break;
+        }
+    }
+
+    public void PlaySoundMusic(SoundMusic sound)
+    {
+        float volume = SaveSystem.optionsData.soundMusique;
+
+        switch (sound)
+        {
+            default:
+                break;
+        }
+    }
+
+    public void PlaySoundAmbiance(SoundAmbiance sound)
+    {
+        float volume = SaveSystem.optionsData.soundAmbiance;
+        
+        switch (sound)
+        {
+            default:
+                break;
+        }
+    }
+}
