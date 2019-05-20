0392ed369a046d8c358061e7043663351374eb50
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2019-05-20 16:27:38 +0200

    Add Collectibles

--- Assets/Scripts/Game/Managers/GameManager.cs
+++ Assets/Scripts/Game/Managers/GameManager.cs
@@ -4,16 +4,55 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    #region Fields
+    private int _currentCollectibles = 0;
+    #endregion
+
+    #region Properties
+    public int CurrentCollectibles { get => _currentCollectibles; }
+    #endregion
+
+    #region MonoBehaviour Callbacks
     void Awake()
     {
         SaveSystem.Load();
 
         ApplySavedSettings();
     }
+    #endregion
 
     public void ApplySavedSettings()
     {
         int vSyncCount = SaveSystem.optionsData.enableVSync ? 2 : 0;
         QualitySettings.vSyncCount = vSyncCount;
     }
+
+    public void AddCollectible()
+    {
+        _currentCollectibles++;
+
+        if (UIManager.Instance != null)
+        {
+            UIManager.Instance.UpdateTextCollectible();
+        }
+    }
+
+#if UNITY_EDITOR
+    void OnGUI()
+    {
+        if (UIManager.Instance != null)
+            return;
+
+        int w = Screen.width, h = Screen.height;
+
+        GUIStyle style = new GUIStyle();
+
+        Rect rect = new Rect(0, 0, w, h * 2 / 100);
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h * 2 / 80;
+        style.normal.textColor = Color.red;
+
+        GUI.Label(rect, "collectible(s) x " + CurrentCollectibles, style);
+    }
+#endif
 }
---
+++ Assets/Scripts/Game/Misc/Collectible.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    [SerializeField] private float _timeToScaleDown = 3f;
+    [SerializeField] private GameObject _prefabDestroyPS = null;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (GameManager.Instance == null)
+            {
+                Debug.LogError("No GameManager in scene!");
+                return;
+            }
+
+            GameManager.Instance.AddCollectible();
+
+            // play fx and destro collectible
+            CharFeedbacks.Instance.PlayJumpPS();
+            StartCoroutine(ScaleDown());
+        }
+    }
+
+    IEnumerator ScaleDown()
+    {
+        float time = Time.time;
+
+        while (transform.localScale == Vector3.zero)
+        {
+            transform.localScale = Vector3.Lerp(Vector3.one, Vector2.zero, (Time.time - time) / _timeToScaleDown);
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        Instantiate(_prefabDestroyPS, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
