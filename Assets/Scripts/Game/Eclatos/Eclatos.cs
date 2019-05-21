ae3e9812e68406b34a18d32400341d955f0b8bf8
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2019-05-21 10:46:05 +0200

    Add Rotation to Eclatos

--- Assets/Scripts/Game/Eclatos/Eclatos.cs
+++ Assets/Scripts/Game/Eclatos/Eclatos.cs
@@ -8,6 +8,7 @@ public class Eclatos : MonoBehaviour
 {
     #region Fields
     [SerializeField] private float _speedSplit = 8f;
+    [SerializeField] private float _rotationSpeed = 90f;
     [Space]
     [SerializeField] private Form _startOnForm = Form.Void;
     [Space]
@@ -15,7 +16,9 @@ public class Eclatos : MonoBehaviour
     [SerializeField] private Transform[] _pointsEtheral = new Transform[4];
 
     private Vector3[] _positionVoid = new Vector3[4];
+    private Vector3[] _eulerAngleVoid = new Vector3[4];
     private Vector3[] _positionEthereal = new Vector3[4];
+    private Vector3[] _eulerAngleEthereal = new Vector3[4];
 
     private Transform[] _points = new Transform[4];
     private static readonly float SPLIT_MIN_DISTANCE = 0.1f;
@@ -31,11 +34,13 @@ public class Eclatos : MonoBehaviour
         for (int i = 0; i < _pointsVoid.Length; i++)
         {
             _positionVoid[i] = _pointsVoid[i].position;
+            _eulerAngleVoid[i] = _pointsVoid[i].eulerAngles;
         }
 
         for (int i = 0; i < _pointsEtheral.Length; i++)
         {
             _positionEthereal[i] = _pointsEtheral[i].position;
+            _eulerAngleEthereal[i] = _pointsEtheral[i].eulerAngles;
         }
 
 
@@ -73,16 +78,16 @@ public class Eclatos : MonoBehaviour
         if (form == Form.Void)
         {
             StopAllCoroutines();
-            StartCoroutine(GotoPoints(_positionVoid));
+            StartCoroutine(GotoPoints(_positionVoid, _eulerAngleVoid));
         }
         else if (form == Form.Ethereal)
         {
             StopAllCoroutines();
-            StartCoroutine(GotoPoints(_positionEthereal));
+            StartCoroutine(GotoPoints(_positionEthereal, _eulerAngleEthereal));
         }
     }
 
-    IEnumerator GotoPoints(Vector3[] destination)
+    IEnumerator GotoPoints(Vector3[] position, Vector3[] rotation)
     {
         Debug.Log("GotoPoints()");
 
@@ -97,16 +102,18 @@ public class Eclatos : MonoBehaviour
                 Transform t = _points[i];
 
                 // if position is to far from destination...
-                if (Vector3.Distance(t.position, destination[i]) >= SPLIT_MIN_DISTANCE)
+                if (Vector3.Distance(t.position, position[i]) >= SPLIT_MIN_DISTANCE)
                 {
                     // ... go to destination.
-                    t.position = Vector3.MoveTowards(t.position, destination[i], Time.deltaTime * _speedSplit);
+                    t.position = Vector3.MoveTowards(t.position, position[i], Time.deltaTime * _speedSplit);
+                    t.eulerAngles = Vector3.MoveTowards(t.eulerAngles, rotation[i], Time.deltaTime * _rotationSpeed);
 
                     isCompleted = false;
                 }
                 else
                 {
-                    t.position = destination[i];
+                    t.position = position[i];
+                    t.eulerAngles = rotation[i];
                 }
             }
 
@@ -123,9 +130,6 @@ public class Eclatos : MonoBehaviour
             {
                 Gizmos.color = Color.green;
                 Gizmos.DrawLine(_pointsVoid[i].position, _pointsEtheral[i].position);
-
-                //Handles.Label(_pointsVoid[i].position, "V1");
-                //Handles.Label(_pointsEtheral[i].position, "E1");
             }
         }
     }
