744406a3af13d2254243278bde6265be4df120ca
Lortedo29 <31354550+Lortedo29@users.noreply.github.com>
2019-05-21 15:03:03 +0200

    Cinematics + Parallax code preparation

---
+++ Assets/Scripts/Game/Misc/TriggerToTransition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TriggerToTransition : MonoBehaviour
+{
+    #region Fields
+    [SerializeField] private bool _hasBeenTriggered = false;
+    #endregion
+
+    #region MonoBehaviour Callbacks
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_hasBeenTriggered)
+            return;
+
+        if (other.CompareTag("Player"))
+        {
+            _hasBeenTriggered = true;
+
+            GameState.state++;
+            SceneManager.LoadScene("transition");
+        }
+    }
+    #endregion
+}
