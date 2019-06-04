713b71e1da18ba9765fb73862ea9133ba259b7dd
Theo Farnole <theo.farnole@gmail.com>
2019-06-04 19:18:54 +0200

    Remove key field in TranslateText

--- Assets/Scripts/Utils/Translation/TranslateText.cs
+++ Assets/Scripts/Utils/Translation/TranslateText.cs
@@ -5,13 +5,12 @@ using UnityEngine.UI;
 [RequireComponent(typeof(TextMeshProUGUI))]
 public sealed class TranslateText : MonoBehaviour
 {
-    [SerializeField]
-    private string m_Key = null;
+    private string _key = null;
 
-    private void Start()
+    void Start()
     {
         var text = GetComponent<TextMeshProUGUI>();
-        text.text = Translation.Get(m_Key != string.Empty ? m_Key : text.text);
+        text.text = Translation.Get(_key != string.Empty ? _key : text.text);
         Destroy(this);
     }
 }
