aa251e73ed72027b48307970576d736d1931b36c
Theo Farnole <theo.farnole@gmail.com>
2019-06-04 21:33:17 +0200

    TranslateText can be updated at any time with new Language

--- Assets/Scripts/Utils/Translation/TranslateText.cs
+++ Assets/Scripts/Utils/Translation/TranslateText.cs
@@ -5,12 +5,30 @@ using UnityEngine.UI;
 [RequireComponent(typeof(TextMeshProUGUI))]
 public sealed class TranslateText : MonoBehaviour
 {
+    #region Fields
     private string _key = string.Empty;
+    private TextMeshProUGUI _text;
+    #endregion
+
+    #region MonoBehaviour Callbacks
+    void Awake()
+    {
+        _text = GetComponent<TextMeshProUGUI>();
+        _key = _key != string.Empty ? _key : _text.text;
+
+        UpdateText();
+    }
 
     void Start()
     {
-        var text = GetComponent<TextMeshProUGUI>();
-        text.text = Translation.Get(_key != string.Empty ? _key : text.text);
-        Destroy(this);
+        LanguageHandle d = new LanguageHandle(UpdateText);
+        UIMenuManager.EventLanguageChangement += d;
+    }
+    #endregion
+
+    void UpdateText()
+    {
+        Debug.Log("event receved");
+        _text.text = Translation.Get(_key);
     }
 }
