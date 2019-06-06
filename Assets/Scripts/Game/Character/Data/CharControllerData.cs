6f798078abb9832ae625d4146bf117a15cc371ce
Theo Farnole <theo.farnole@gmail.com>
2019-06-06 11:20:49 +0200

    Dash Inertia

--- Assets/Scripts/Game/Character/Data/CharControllerData.cs
+++ Assets/Scripts/Game/Character/Data/CharControllerData.cs
@@ -5,33 +5,39 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Erebos/CharController")]
 public class CharControllerData : ScriptableObject
 {
-    [Header("Run settings")]
+    [Header("Walk")]
     [SerializeField] private float _speed = 3f;
     [SerializeField] private float _maxVelocityOnX = 10f;
     [Space]
     [SerializeField] private float _turnSpeed = 60f;
-    [Header("Jumps settings")]
+    [Header("Jump")]
     [SerializeField] private float _jumpHeight = 4;
+    [Header("Dash")]
     [SerializeField] private float _dashDistance = 8;
     [SerializeField] private float _dashTime = 8;
     [Space]
+    [SerializeField] private float _dashInertia = 1;
+    [Header("Sticking")]
     [SerializeField] private float _stickedJumpForce = 20;
     [SerializeField, Range(0, 90)] private float _stickedJumpAngle = 45f;
-    [Space]
+    [Header("Air")]
     [SerializeField] private float _fallMultiplier = 2.5f;
     [SerializeField] private float _airControlSpeed = 1f;
     
 
     public float Speed { get => _speed; }
     public float MaxVelocityOnX { get => _maxVelocityOnX;  }
+    public float TurnSpeed { get => _turnSpeed; }
+
+    public float JumpHeight { get => _jumpHeight;}
+
+    public float DashDistance { get => _dashDistance; }
+    public float DashTime { get => _dashTime;  }
+    public float DashInertia { get => _dashInertia; }
 
     public float StickedJumpForce { get => _stickedJumpForce; }
+    public float StickedJumpAngle { get => _stickedJumpAngle; }
 
     public float AirControlSpeed { get => _airControlSpeed; }
     public float FallMultiplier { get => _fallMultiplier; }
-    public float JumpHeight { get => _jumpHeight;}
-    public float DashDistance { get => _dashDistance; }
-    public float StickedJumpAngle { get => _stickedJumpAngle; }
-    public float DashTime { get => _dashTime;  }
-    public float TurnSpeed { get => _turnSpeed; }
 }
