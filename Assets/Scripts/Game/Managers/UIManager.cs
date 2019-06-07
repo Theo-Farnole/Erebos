6050482583e647031dd3b12e2b5a25204bbc8ddf
Theo Farnole <theo.farnole@gmail.com>
2019-06-07 19:28:21 +0200

    Tutorial fadeIn

--- Assets/Scripts/Game/Managers/UIManager.cs
+++ Assets/Scripts/Game/Managers/UIManager.cs
@@ -3,16 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class UIManager : Singleton<UIManager>
 {
+
     #region Fields
     [Header("-- InGame UI ")]
     [SerializeField] private TextMeshProUGUI _textCollectible;
     [SerializeField] private Image _imageCollectible;
     [Space]
-    [SerializeField] private float _fadeoutTime = 2.3f;
+    [SerializeField] private float _collectiblesFadeoutTime = 2.3f;
     [Header("-- Pause UI ")]
     [SerializeField] private GameObject _panelPause;
     [Space]
@@ -20,11 +22,18 @@ public class UIManager : Singleton<UIManager>
     [SerializeField] private TextMeshProUGUI[] _textSpeedRunsTimes = new TextMeshProUGUI[3];
     [SerializeField] private TextMeshProUGUI[] _textDeathCount = new TextMeshProUGUI[3];
     [Header("-- Pause UI - Buttons")]
+    [SerializeField] private Button _buttonContinue;
+    [SerializeField] private Button _buttonStats;
     [SerializeField] private Button _buttonRestart;
-    [SerializeField] private Button _buttonQuitPause;
+    [SerializeField] private Button _buttonQuit;
     [Header("-- Feathers")]
+    [SerializeField] private float _tutorialFadeInTime = 1f;
+    [Space]
     [SerializeField] private GameObject _panelWhiteFeather;
     [SerializeField] private GameObject _panelBlackFeather;
+    [Space]
+    [SerializeField] private TextMeshProUGUI _textWhiteFeather;
+    [SerializeField] private TextMeshProUGUI _textBlackFeather;
     #endregion
 
     #region MonoBehaviour Callbacks
@@ -32,8 +41,20 @@ public class UIManager : Singleton<UIManager>
     {
         _panelPause.SetActive(false);
 
-        _buttonRestart.onClick.AddListener(() => GameManager.Instance.RestartCheckpoint());
-        _buttonQuitPause.onClick.AddListener(() => UpdatePanelPause());
+        // pause button
+        _buttonContinue.onClick.AddListener(() =>
+        {
+            Time.timeScale = 1;
+            UpdatePanelPause();
+        });
+        _buttonRestart.onClick.AddListener(() =>
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        });
+
+        //_buttonStats.onClick.AddListener(() => UpdatePanelPause());
+        _buttonQuit.onClick.AddListener(Application.Quit);
 
         _panelWhiteFeather.SetActive(false);
         _panelBlackFeather.SetActive(false);
@@ -71,8 +92,8 @@ public class UIManager : Singleton<UIManager>
         _textCollectible.gameObject.SetActive(true);
         _imageCollectible.gameObject.SetActive(true);
 
-        _textCollectible.Fade(FadeType.FadeOut, _fadeoutTime);
-        _imageCollectible.Fade(FadeType.FadeOut, _fadeoutTime);
+        _textCollectible.Fade(FadeType.FadeOut, _collectiblesFadeoutTime);
+        _imageCollectible.Fade(FadeType.FadeOut, _collectiblesFadeoutTime);
     }
     #endregion
 
@@ -81,13 +102,21 @@ public class UIManager : Singleton<UIManager>
         switch (unlockedForm)
         {
             case Form.Ethereal:
-                _panelWhiteFeather.SetActive(true);
                 Time.timeScale = 0;
+
+                _panelWhiteFeather.SetActive(true);
+
+                _panelWhiteFeather.GetComponent<Image>().Fade(FadeType.FadeIn, _tutorialFadeInTime);
+                _textWhiteFeather.Fade(FadeType.FadeIn, _tutorialFadeInTime);
                 break;
 
             case Form.Void:
-                _panelBlackFeather.SetActive(true);
                 Time.timeScale = 0;
+
+                _panelBlackFeather.SetActive(true);
+
+                _panelBlackFeather.GetComponent<Image>().Fade(FadeType.FadeIn, _tutorialFadeInTime);
+                _textBlackFeather.Fade(FadeType.FadeIn, _tutorialFadeInTime);
                 break;
         }
     }
@@ -96,27 +125,6 @@ public class UIManager : Singleton<UIManager>
     {
         GameState.UpdateCurrentSpeedrunTime();
         bool isInPause = (Time.timeScale == 0);
-
-        if (isInPause)
-        {
-            for (int i = 0; i < GameState.speedrunTime.Length; i++)
-            {
-                float t = GameState.speedrunTime[i];
-
-                int min = Mathf.FloorToInt(t / 60);
-                int sec = Mathf.FloorToInt(t % 60);
-
-                string txt = string.Format(min.ToString("00") + ":" + sec.ToString("00"));
-
-                _textSpeedRunsTimes[i].text = txt;
-            }
-
-            for (int i = 0; i < GameState.speedrunTime.Length; i++)
-            {
-                float d = GameState.deathCount[i];
-                _textDeathCount[i].text = d.ToString();
-            }
-        }
 
         _panelPause.SetActive(isInPause);
     }
