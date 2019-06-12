49bd98a394eec46a417209dc8da1ce89ca732499
Theo Farnole <theo.farnole@gmail.com>
2019-06-12 10:06:10 +0200

    add FALL_TRIGGER_MINIMUN_Y in CharController

--- Assets/Scripts/Game/Character/CharController.cs
+++ Assets/Scripts/Game/Character/CharController.cs
@@ -19,6 +19,7 @@ public class CharController : MonoBehaviour
     public static readonly int MAX_JUMPS = 2;
     public static readonly Vector3 HEAD_POSITION = 0.7f * Vector3.up;
     public static readonly float RADIUS_GROUNDED_SPHERE = 0.4f;
+    public static readonly float FALL_TRIGGER_MIN_Y = 0.1f;
 
     #region Fields
     class PlayerCollision
@@ -483,7 +484,7 @@ public class CharController : MonoBehaviour
         if (Time.timeScale == 0)
             return;
 
-        bool isFalling = _rigidbody.velocity.y < 0;
+        bool isFalling = _rigidbody.velocity.y < FALL_TRIGGER_MIN_Y;
         bool isGrounded = _collision.down;
         float velocity = 0;
 
